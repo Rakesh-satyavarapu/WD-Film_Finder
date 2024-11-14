221f26aefe21f1dbdc033f2acd1570233fdb1c62
Rakesh-satyavarapu <rakeshsatyavarapu369@gmail.com>
2024-11-14 23:52:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ function moviedata ()
 {
 let title=document.getElementById('Title').value;
 const API_KEY='9b97a8a0';
-const apiUrl = `https://www.omdbapi.com/?t=${title}&apikey=${API_KEY}`;
-fetch(apiUrl)
+fetch(`http://www.omdbapi.com/?t=${title}&apikey=${API_KEY}`)
 .then(response=>response.json())
 .then(data=>{
 document.getElementById('movieDetails').innerHTML=`
