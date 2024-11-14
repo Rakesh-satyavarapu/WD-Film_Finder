bba1c66e0a7e3cf707105ef06fc65643f2aa28b5
Rakesh-satyavarapu <rakeshsatyavarapu369@gmail.com>
2024-11-14 22:30:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function moviedata ()
+{
+let title=document.getElementById('Title').value
+fetch(`http://www.omdbapi.com/?t=${title}&apikey=${API_KEY}`)
+.then(response=>response.json())
+.then(data=>{
+document.getElementById('movieDetails').innerHTML=`
+<div class="container">
+<div id="imgalign"><img src='${data.Poster}'></div>
+<div id="data">
+<table>
+<tr id="title"><th>Title: </th><td>${data.Title}</td> </tr><br>
+<tr id="rating"><th>IMDB Rating: </th><td>${data.imdbRating}</td></tr><br>
+<tr id="plot"><th>Plot: </th><td>${data.Plot}</td></tr><br>
+<tr id="releasedate"><th>ReleasedOn: </th><td>${data.Released}</td></tr><br>
+<tr id="director"><th>Director: </th><td>${data.Director}</td></tr><br>
+<tr id="writer"><th>Writer: </th><td>${data.Writer}</td></tr><br>
+<tr id="actors"><th>Actors: </th><td>${data.Actors}</td></tr> 
+</table>
+</div>
+</div>`;
+})
+.catch(error=>{console.log('error',error)})
+}
